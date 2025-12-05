fa34b1be1ce369c0c48a7b351870bdc39d517206
reda cherfaoui <redacherfaoui@gmail.com>
2025-12-05 20:17:35 +0100

    feat: modifications avant bump version

--- src/MyDemo.vue
+++ src/MyDemo.vue
@@ -5,7 +5,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="border-2 border-red-500 p-4 rounded-lg bg-red-100 text-red-900">
+  <div class="border-2 border-blue-500 p-4 rounded-lg bg-blue-100 text-blue-900">
     <h3 class="font-bold text-lg mb-2">Web UI Demo Component</h3>
     <p class="mb-2">{{ msg || 'Hello from web-ui!' }}</p>
     <p>Lorem ipsum dolor sit amet. Green Background with Tailwind two</p>
